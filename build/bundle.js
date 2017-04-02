2d85bf3925110ace2aa769de5535d72eab3ae759
notryanb <notryanb@gmail.com>
2017-04-01 23:29:19 -0400

    Add colors

--- build/bundle.js
+++ build/bundle.js
@@ -82,8 +82,9 @@ var Graph = function () {
     this.ctx = this.canvas.getContext('2d');
     this.imageData = this.ctx.createImageData(this.canvas.width, this.canvas.height);
     this.aspectRatio = this.canvas.height / this.canvas.width;
-    this.r = 4;
+    this.r = 20;
     this.center = { x: 0, y: 0 };
+    this.iterations = 200;
     this.indexToCoord = this.indexToCoord.bind(this);
     this.render = this.render.bind(this);
   }
@@ -105,17 +106,13 @@ var Graph = function () {
   }, {
     key: 'render',
     value: function render(predicate) {
-      //for (let i = 0; i < this.imageData.data.length; i += 1) {
-      //this.imageData.data[i] = Math.random() * 255;
-      //}
-      //this.ctx.putImageData(this.imageData, 0, 0);
-      //
       for (var i = 0; i < this.canvas.width * this.canvas.height * 4; i += 4) {
-        set = predicate(this.indexToCoord(i)) ? 255 : 0;
-        this.imageData.data[i] = 0;
-        this.imageData.data[i + 1] = 0;
-        this.imageData.data[i + 2] = 0;
-        this.imageData.data[i + 3] = set;
+        var val = predicate(this.indexToCoord(i));
+        var set = val[0] ? 0 : val[1] / this.iterations * 0xffffff;
+        this.imageData.data[i] = (set & 0xff0000) >> 16;
+        this.imageData.data[i + 1] = (set & 0x00ff00) >> 8;
+        this.imageData.data[i + 2] = set & 0x0000ff;
+        this.imageData.data[i + 3] = 255;
       }
       this.ctx.putImageData(this.imageData, 0, 0);
     }
@@ -129,10 +126,11 @@ var isMandlebrot = function isMandlebrot(coord) {
   var ci = coord.y;
   var zr = cr;
   var zi = ci;
+  var i = void 0;
 
-  for (var i = 0; i < 100; i++) {
+  for (i = 0; i < 200; i++) {
     if (zr ** 2 + zi ** 2 > 4) {
-      return false;
+      return [false, i];
     }
 
     newZr = zr * zr - zi * zi + cr;
@@ -140,10 +138,13 @@ var isMandlebrot = function isMandlebrot(coord) {
     zr = newZr;
     zi = newZi;
   }
-  return true;
+  return [true, i];
 };
 
 var graph = new Graph('mandlebrot');
+graph.center.x = -0.7453;
+graph.center.y = 0.1127;
+graph.r = 0.00015;
 graph.render(isMandlebrot);
 
 /***/ })
--- index.js
+++ index.js
@@ -4,8 +4,9 @@ class Graph {
     this.ctx = this.canvas.getContext('2d');
     this.imageData = this.ctx.createImageData(this.canvas.width, this.canvas.height);
     this.aspectRatio = this.canvas.height / this.canvas.width;
-    this.r = 4;
+    this.r = 20
     this.center = { x: 0, y: 0 };
+    this.iterations = 200;
     this.indexToCoord = this.indexToCoord.bind(this);
     this.render = this.render.bind(this);
   }
@@ -28,17 +29,13 @@ class Graph {
   }
 
   render(predicate) {
-    //for (let i = 0; i < this.imageData.data.length; i += 1) {
-      //this.imageData.data[i] = Math.random() * 255;
-    //}
-    //this.ctx.putImageData(this.imageData, 0, 0);
-    //
     for (let i = 0; i < this.canvas.width * this.canvas.height * 4; i += 4) {
-      set = predicate(this.indexToCoord(i)) ? 255 : 0;
-      this.imageData.data[i] = 0;
-      this.imageData.data[i + 1] = 0;
-      this.imageData.data[i + 2] = 0;
-      this.imageData.data[i + 3] = set; 
+      let val = predicate(this.indexToCoord(i));
+      let set = val[0] ? 0 : (val[1] / this.iterations) * 0xffffff;
+      this.imageData.data[i] = (set & 0xff0000) >> 16;
+      this.imageData.data[i + 1] = (set & 0x00ff00) >> 8;
+      this.imageData.data[i + 2] = set & 0x0000ff;
+      this.imageData.data[i + 3] = 255; 
     }
     this.ctx.putImageData(this.imageData, 0, 0);
   }
@@ -49,10 +46,11 @@ const isMandlebrot = (coord) => {
   let ci = coord.y;
   let zr = cr;
   let zi = ci;
+  let i;
 
-  for(let i = 0; i < 100; i++) {
+  for(i = 0; i < 200; i++) {
     if (zr**2 + zi**2 > 4) {
-      return false;
+      return [false, i];
     }
 
     newZr = (zr * zr) - (zi * zi) + cr;
@@ -60,8 +58,11 @@ const isMandlebrot = (coord) => {
     zr = newZr;
     zi = newZi;
   }
-  return true;
+  return [true, i];
 }
 
 let graph = new Graph('mandlebrot');
+graph.center.x = -0.7453;
+graph.center.y = 0.1127;
+graph.r = 0.00015;
 graph.render(isMandlebrot);
